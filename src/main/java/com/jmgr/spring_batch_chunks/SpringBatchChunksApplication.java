79df9bbee703187f833ca384ee19c7e8a6e1cbd1
Jesus Martin Garcia Ramirez <jmgramirez@minsait.com>
2026-05-19 17:11:40 -0600

    initial commit

---
+++ src/main/java/com/jmgr/spring_batch_chunks/SpringBatchChunksApplication.java
@@ -0,0 +1,13 @@
+package com.jmgr.spring_batch_chunks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchChunksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatchChunksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jmgr/spring_batch_chunks/SpringBatchChunksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jmgr.spring_batch_chunks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchChunksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
